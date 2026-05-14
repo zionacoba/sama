04ce43fa555fdd1a14613c10c29fb3086189167b
zionacoba <acobapaulzion@gmail.com>
2026-05-13 20:24:36 -0700

    fix navbar name to Sama

--- app/page.tsx
+++ app/page.tsx
@@ -94,7 +94,7 @@ export default function Home() {
               href="/"
               className="text-lg font-bold tracking-tight text-trailhead"
             >
-              ⛰ Trailhead
+              ⛰Sama
             </a>
             <a
               href="#"
